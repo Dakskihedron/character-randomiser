7d16c3ba0f6d5a4927efc552f4c9e1c386a312a9
Dakskihedron <GhastHunter352@gmail.com>
2020-08-03 11:52:33 +1200

    Added function that creates html elements (wip)

--- src/render.js
+++ src/render.js
@@ -3,15 +3,21 @@ const { join } = require('path')
 // Declare HTML elements
 const genButton = document.getElementById('gen-button')
 const rpgSelect = document.getElementById('rpg-selection')
-const output = document.getElementById('output')
 
 genButton.addEventListener('click', () => {
-    let rpgName = rpgSelect.options[rpgSelect.selectedIndex].value
-    let rpgFile = require(join(__dirname, '../resources/rpgs', `${rpgName}.json`))
-    const newline = '\n'
-    let valuesList = []
-    Object.keys(rpgFile).forEach(function(k) {
-        valuesList.push(`${k}: ${Math.floor(Math.random() * rpgFile[k]) + 1}`)
-        output.value = valuesList.join(newline)
-    })
+    let statBox = document.createElement('div')
+    let statContent = document.createTextNode("Hello world!")
+    statBox.appendChild(statContent)
+    document.getElementById('output-container').appendChild(statBox)
 })
+
+// genButton.addEventListener('click', () => {
+//     let rpgName = rpgSelect.options[rpgSelect.selectedIndex].value
+//     let rpgFile = require(join(__dirname, '../resources/rpgs', `${rpgName}.json`))
+//     const newline = '\n'
+//     let valuesList = []
+//     Object.keys(rpgFile).forEach(function(k) {
+//         valuesList.push(`${k}: ${Math.floor(Math.random() * rpgFile[k]) + 1}`)
+//         output.value = valuesList.join(newline)
+//     })
+// })
