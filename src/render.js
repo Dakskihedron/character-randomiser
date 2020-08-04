c199405d31e039b8ea39d9b19fe0744e3dde64f3
Dakskihedron <GhastHunter352@gmail.com>
2020-08-05 00:08:22 +1200

    Generate button now creates a collapsible element, added collapsible element handler

--- src/render.js
+++ src/render.js
@@ -1,15 +1,37 @@
 const { join } = require('path')
 
 // Declare HTML elements
-const genButton = document.getElementById('gen-button')
-const rpgSelect = document.getElementById('rpg-selection')
+const genButton = document.getElementById('generate-button')
+const rpgSelect = document.getElementById('rpg-select')
+const output = document.getElementById('output-container')
 
+// Handles the generate button
 genButton.addEventListener('click', () => {
-    let statBox = document.createElement('div')
-    let statContent = document.createTextNode("Hello world!")
-    statBox.appendChild(statContent)
-    document.getElementById('output-container').appendChild(statBox)
+  const collapsible = document.createElement('button')
+  collapsible.setAttribute('class', 'collapsible')
+  const content = document.createElement('div')
+  content.setAttribute('class', 'content')
+  const text = document.createTextNode('Hello world!')
+  content.appendChild(text)
+  output.appendChild(collapsible)
+  output.appendChild(content)
 })
+
+// Handles collapsible elements
+var coll = document.getElementsByClassName("stat-collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 // genButton.addEventListener('click', () => {
 //     let rpgName = rpgSelect.options[rpgSelect.selectedIndex].value
