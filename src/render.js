aef7ac8aec1b2892c6f5f946870c027ce243e813
Dakskihedron <GhastHunter352@gmail.com>
2020-08-06 16:03:41 +1200

    Made changes to code for creating collapsibles and code for handling collapsibles

--- src/render.js
+++ src/render.js
@@ -8,30 +8,31 @@ const output = document.getElementById('output-container')
 // Handles the generate button
 genButton.addEventListener('click', () => {
   const collapsible = document.createElement('button')
-  collapsible.setAttribute('class', 'collapsible')
+  collapsible.setAttribute('class', "collapsible")
   const content = document.createElement('div')
-  content.setAttribute('class', 'content')
-  const text = document.createTextNode('Hello world!')
-  content.appendChild(text)
+  content.setAttribute('class', "content")
+  const btext = document.createTextNode("Open collapsible")
+  collapsible.appendChild(btext)
+  const p = document.createElement('p')
+  const text = document.createTextNode("Hello world!")
+  p.appendChild(text)
+  content.appendChild(p)
   output.appendChild(collapsible)
   output.appendChild(content)
 })
 
 // Handles collapsible elements
-var coll = document.getElementsByClassName("stat-collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+document.querySelector('#output-container').addEventListener('click', function(e) {
+  if (e.target.classList.contains('collapsible')) {
+    e.target.classList.toggle('active')
+    let content = e.target.nextElementSibling
+    if (content.style.display === 'block') {
+      content.style.display = 'none'
     } else {
-      content.style.display = "block";
+      content.style.display = 'block'
     }
-  });
-}
+  }
+})
 
 // genButton.addEventListener('click', () => {
 //     let rpgName = rpgSelect.options[rpgSelect.selectedIndex].value
