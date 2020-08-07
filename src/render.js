c68a3bcf820cca8d2ae53848786d499e052994a7
Dakskihedron <GhastHunter352@gmail.com>
2020-08-07 21:08:55 +1200

    Made changes to collapsible element creator and added code for parsing the rpg files

--- src/render.js
+++ src/render.js
@@ -1,4 +1,5 @@
 const { join } = require('path')
+const { unlink } = require('fs')
 
 // Declare HTML elements
 const genButton = document.getElementById('generate-button')
@@ -7,18 +8,38 @@ const output = document.getElementById('output-container')
 
 // Handles the generate button
 genButton.addEventListener('click', () => {
-  const collapsible = document.createElement('button')
-  collapsible.setAttribute('class', "collapsible")
-  const content = document.createElement('div')
-  content.setAttribute('class', "content")
-  const btext = document.createTextNode("Open collapsible")
-  collapsible.appendChild(btext)
-  const p = document.createElement('p')
-  const text = document.createTextNode("Hello world!")
-  p.appendChild(text)
-  content.appendChild(p)
-  output.appendChild(collapsible)
-  output.appendChild(content)
+  const nl = '\n'
+  let parsedList = []
+  let filename = rpgSelect.options[rpgSelect.selectedIndex].value
+  let file = require(join(__dirname, '../resources/rpgs', `${filename}.json`))
+  Object.keys(file).forEach(function(k) {
+    parsedList.push(`${k}: ${Math.floor(Math.random() * file[k]) + 1}`)
+  })
+  if (parsedList) {
+    // Create collapsible
+    const collapsible = document.createElement('button')
+    collapsible.setAttribute('class', 'collapsible')
+    // Create collapsible content container
+    const content = document.createElement('div')
+    content.setAttribute('class', 'content')
+    // Set collapsible title
+    let collTitle = document.createTextNode(filename)
+    collapsible.appendChild(collTitle)
+    // Create collapsible content
+    const ul = document.createElement('ul')
+    ul.setAttribute('class', 'list')
+    parsedList.forEach((element) => {
+      const li = document.createElement('li')
+      li.appendChild(document.createTextNode(element))
+      ul.appendChild(li)
+    })
+    // Append stuff
+    content.appendChild(ul)
+    output.appendChild(collapsible)
+    output.appendChild(content)
+  } else {
+    alert("ERROR: An exception has occurred!")
+  }
 })
 
 // Handles collapsible elements
@@ -33,14 +54,3 @@ document.querySelector('#output-container').addEventListener('click', function(e
     }
   }
 })
-
-// genButton.addEventListener('click', () => {
-//     let rpgName = rpgSelect.options[rpgSelect.selectedIndex].value
-//     let rpgFile = require(join(__dirname, '../resources/rpgs', `${rpgName}.json`))
-//     const newline = '\n'
-//     let valuesList = []
-//     Object.keys(rpgFile).forEach(function(k) {
-//         valuesList.push(`${k}: ${Math.floor(Math.random() * rpgFile[k]) + 1}`)
-//         output.value = valuesList.join(newline)
-//     })
-// })
